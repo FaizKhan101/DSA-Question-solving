1e92dd4b6dac800ffd6a74254d9a2cc4940d50a6
Faiz <faizkhan8225@gmail.com>
2023-09-02 01:16:37 +0530

    Add recursion Folder and in there write fibonacci function

---
+++ recursion/fibonacci.js
@@ -0,0 +1,21 @@
+// Write a function for findind fibonacci number.
+
+function fibonacci(num) {
+  if (num === 0) return 0;
+  else if (num === 1) return 1;
+  else return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+// just return final number
+console.log(fibonacci(20));
+
+// for complete list of fibonacci
+// let target = 10;
+// let fibArray = [];
+// for (let i = 0; i <= target; i++) {
+//   fibArray.push(fibonacci(i));
+// }
+
+// console.log(fibArray);
+
+// 0,1,1,2,3,5,8,13...
