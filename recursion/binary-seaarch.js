6cae3db411a6b27b3d6f588b794d34c791f6e60d
Faiz <faizkhan8225@gmail.com>
2023-09-03 14:43:41 +0530

    Add binary search in recursion

---
+++ recursion/binary-seaarch.js
@@ -0,0 +1,18 @@
+// Write a recursive function for binary search
+
+function binarySearch(arr, target) {
+    if (arr.length === 0) return false;
+
+    arr.sort((a,b) => a -b)
+    console.log(arr);
+    const mid = Math.floor(arr.length / 2)
+
+    if ( arr[mid] === target) return true;
+    else if (arr[mid] < target) return binarySearch(arr.slice(mid + 1), target)
+    else return binarySearch(arr.slice(0, mid), target)
+}
+
+let arr = [4, 3, 6, 5, 1, -5, -3, 6, -1, 12];
+let target = 5;
+
+console.log(binarySearch(arr, target));
